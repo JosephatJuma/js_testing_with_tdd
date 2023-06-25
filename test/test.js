e980c523fb2daa8d22f8a3ac9d811d7f212b6eea
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-25 22:53:46 +0300

    second test failed, one not matching four

--- test/test.js
+++ test/test.js
@@ -1,5 +1,3 @@
 const assert = require("chai").assert;
-function multiply(a, b) {
-  return 1;
-}
-assert.equal(multiply(1, 1), 1);
+const multiply = require("../multiply");
+assert.equal(multiply(2, 2), 4);
