fd34b2ee5f82289bf1faf68fa199b2bff5ab16eb
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-25 23:09:38 +0300

    Last test running

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
 const assert = require("chai").assert;
 const multiply = require("../multiply");
-assert.equal(multiply(23, 45), 23 * 45);
+
+describe("multiply", function () {
+  it("Oh! Its still green", function () {
+    assert.equal(multiply(23, 45), 23 * 45);
+  });
+});
